bbcbd27ea9f8e1d3546e9ee97dbbcd6e10d7ff48
Sourav Sahoo <jackysourav1994@gmail.com>
2022-12-20 20:36:53 +0530

    Created SpringWebApplicationInitializer and ApplicationSecurityConfig

---
+++ Shop_Recommendation_System/src/com/souravsahoo/SRSproj/config/ApplicationSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.souravsahoo.SRSproj.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Here we will specify the type of authentication to be done for the application
+ * @author jacky
+ *
+ */
+@Configuration
+public class ApplicationSecurityConfig{
+
+	
+	
+}
---
+++ Shop_Recommendation_System/src/com/souravsahoo/SRSproj/config/SpringWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.souravsahoo.SRSproj.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
